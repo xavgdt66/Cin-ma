fab1baf04cee0abe28b68766bc7ca86a368dd078
xavgdt66 <xaviergodet66100@gmail.com>
2024-03-28 19:34:56 +0100

    Affichage des users avec le role-cinema sur la homepage / UserRepository/User.php/HomeController.php/home/index.html.twig

--- src/Form/MovieType.php
+++ src/Form/MovieType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Movie;
 use App\Entity\DateDiffusion; 
+use App\Form\DateDiffusionType;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
