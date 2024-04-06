eaae06e1a32d4a8ec682de3e371f3f2bfcd346ca
xavgdt66 <xaviergodet66100@gmail.com>
2024-04-06 22:48:37 +0200

    easyadmin + crudAdminUser

---
+++ src/Controller/Admin/MovieCrudController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Movie;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+
+
+class MovieCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Movie::class;
+    }
+
+    
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('titre'),
+            TextEditorField::new('description'),
+            
+
+        ];
+    }
+    
+}
